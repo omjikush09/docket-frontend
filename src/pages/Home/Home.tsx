be4912fef0d1fde3295dbb5d43cdd032706ecb24
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-01-25 22:45:25 +0530

    feat: Made the Pages

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Home.scss'
+const Home = () => {
+  return <div>This is Home Page</div>;
+};
+
+export default Home;
---
+++ src/pages/auth/SignUp.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './SignUp.scss'
+const SignUp = () => {
+  return <div>This is Signup page</div>;
+};
+
+export default SignUp;
